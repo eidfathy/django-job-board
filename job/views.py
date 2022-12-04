38963a1dca859b2cfc6c2c2982b1383f052da387
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-04 19:39:10 +0200

    --- Auto-Git Commit ---

--- job/views.py
+++ job/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 
 def job_list(request):
     job_list = Job.objects.all()
-    context = {'jobs' : job_list} # template name
+    paginator = Paginator(job_list, 1) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'jobs' : page_obj} # template name
     return render(request, 'job/job_list.html', context)
 
 
