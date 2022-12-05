1416ca8a7e7d6607e1ff194f4f9f354709888e4a
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-05 16:56:34 +0200

    --- Auto-Git Commit ---

--- project/settings.py
+++ project/settings.py
@@ -118,11 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'django_project/static')
+    BASE_DIR / "static",
+    '/var/www/static/',
 ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'django_project/static')
+# ]
 
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
