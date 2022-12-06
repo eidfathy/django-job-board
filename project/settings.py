b1d53f80a018a3157bdec251790c5f065f9147dc
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-06 23:08:55 +0200

    --- add login form ---

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     # my apps
     'job',
     'bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
