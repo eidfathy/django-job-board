93bd393c5aa866069d247778accfa48dd86edd9d
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-06 20:22:47 +0200

    --- add apply form ---

--- job/models.py
+++ job/models.py
@@ -51,3 +51,16 @@ class Category(models.Model):
     
     def __str__(self) -> str:
         return self.name
+    
+    
+class Apply(models.Model):
+    job = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name 
--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Job
 from django.core.paginator import Paginator
+from .form import ApplyForm
 
 # Create your views here.
 
@@ -16,5 +17,16 @@ def job_list(request):
 
 def job_detail(request, slug):
     job_detail = Job.objects.get(slug = slug)
-    context = { 'job' : job_detail}
+    
+    if request.method=='POST':
+        form = ApplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.job = job_detail
+            myform.save()
+    
+    else:
+        form = ApplyForm()
+    
+    context = { 'job' : job_detail, 'form' : form}
     return render(request, 'job/job_detail.html', context)
