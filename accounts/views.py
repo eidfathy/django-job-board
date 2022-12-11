68b2d635244306bfc3b694d8e7ed9f269f92bd92
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-11 16:12:10 +0200

    --- add edit profile user ---

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
+from audioop import reverse
 from django.shortcuts import render, redirect
-from .form import SignupForm
+from .form import SignupForm, UserForm, ProfileForm
 from django.contrib.auth import authenticate, login
 from .models import Profile
 # Create your views here.
@@ -18,12 +19,27 @@ def signup(request):
     else:
         form = SignupForm()
     
-    return render(request,'registration/signup.html',{'form' : form})
+    return render(request,'registration/signup.html',{'form':form})
 
 def profile(request):
     profile = Profile.objects.get(user = request.user)
-    return render(request, 'accounts/profile.html', {'profile' : profile})
+    return render(request, 'accounts/profile.html', {'profile':profile})
 
 def profilr_edit(request):
+    profile = Profile.objects.get(user = request.user)
     
-    return render(request, 'accounts/profile_edit.html', {})
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(request.POST, request.FILES, instance=profile,)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect('/accounts/profile')
+            # return redirect(reverse('accounts:profile'))
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+    
+    return render(request, 'accounts/profile_edit.html', {'userform':userform , 'profileform':profileform})
