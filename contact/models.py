755cbf7bc520960b7b1da212d4a58bfce5de3f4d
Eid Fathy <119596741+eidfathy@users.noreply.github.com>
2022-12-13 16:30:19 +0200

    --- contact ---

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Info(models.Model):
+    place = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    class Meta:
+        verbose_name = ("Info")
+        verbose_name_plural = ("Infos")
+
+    def __str__(self):
+        return self.email
